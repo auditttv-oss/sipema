964b96f0cd60bcf38b8f23742748b8500a459c66
auditttv-oss <auditttv@gmail.com>
2026-02-18 16:42:27 +0700

    Fix Vercel build by converting PostCSS/Tailwind configs to ESM

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  plugins: [
-    require('tailwindcss')('./tailwind.config.cjs'),
-    require('autoprefixer'),
-  ],
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [tailwindcss(), autoprefixer()],
 };
